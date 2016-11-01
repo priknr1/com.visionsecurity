051b99e203c8fa081f68f68c54afd7c00a85aeb7
SergeRegoor <serge@regoor.nl>
2016-11-01 20:52:41 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,11 @@
+# Vision Security devices for Homey
+
+Adds support for Vision Security devices in Homey.
+
+## Supported Devices
+
+* ZM1601 Battery Operated Siren
+
+## Revision History
+
+2016-11-01 - Initial version.
---
+++ drivers/ZM1601/driver.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const path = require('path');
+const ZwaveDriver = require('homey-zwavedriver');
+
+// http://www.pepper1.net/zwavedb/device/344
+
+module.exports = new ZwaveDriver(path.basename(__dirname), {
+	capabilities: {
+		'onoff': {
+			'command_class': 'COMMAND_CLASS_SWITCH_BINARY',
+			'command_get': 'SWITCH_BINARY_GET',
+			'command_set': 'SWITCH_BINARY_SET',
+			'command_set_parser': value => {
+				return {
+					'Switch Value': (value > 0) ? 255 : 0
+				};
+			},
+			'command_report': 'SWITCH_BINARY_REPORT',
+			'command_report_parser': report => report['Value'] === 'on/enable'
+		},
+		'measure_battery': {
+			'command_class': 'COMMAND_CLASS_BATTERY',
+			'command_get': 'BATTERY_GET',
+			'command_report': 'BATTERY_REPORT',
+			'command_report_parser': report => {
+				if (report['Battery Level'] === "battery low warning") return 1;
+				
+				return report['Battery Level (Raw)'][0];
+			}
+		}
+	},
+	settings: {
+		'siren_strobe_mode': {
+			index: 1,
+			size: 1
+		},
+		'alarm_auto_stop': {
+			index: 2,
+			size: 1
+		}
+	}
+});
